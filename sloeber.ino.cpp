c3176a868da1bfcde74fa20d433d90093db3926e
zwax <zwax@meo-energy.com>
2021-05-24 16:22:51 +0200

    Revert "ignore auto generated sloeber.ino.cpp"
    This reverts commit 9e34cf4bd28989d18ea29770c106dcf25d3229c3.

---
+++ sloeber.ino.cpp
@@ -0,0 +1,35 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2020-07-03 12:06:43
+
+#include "Arduino.h"
+#include "Arduino.h"
+#include "PID_v1.h"
+#include "Wire.h"
+#include "MotorController.h"
+#include "string.h"
+
+void encoderSignal() ;
+void setMotorPwm(double value) ;
+void stop() ;
+void home() ;
+void setPosTunings( double kp, double ki, double kd ) ;
+void setSpeedTunings( double kp, double ki, double kd ) ;
+void setMaxSpeed(double maxOut) ;
+void runAtSpeed(double speed) ;
+void moveToPos(int pos) ;
+void dumpState() ;
+void twiRequest() ;
+void twiReceive(int numBytes) ;
+void serialReceive() ;
+void calcSpeed(unsigned long timeChange) ;
+void setup() ;
+void speedControl() ;
+void loop() ;
+
+#include "MotorController.ino"
+
+
+#endif
